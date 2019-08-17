2b24f23f563d0c77878fa397ddacbb24b0679348
phtn <phtn458@gmail.com>
2019-08-17 11:07:27 -0400

    added_motion_to_get

--- src/components/get.tsx
+++ src/components/get.tsx
@@ -7,27 +7,26 @@ const GetProduct: FC = () => {
   const [state] = useContext(UIContext);
   const { nightmode, WIDTH, HEIGHT, imageHeight, imageWidth } = state;
   return (
-    <motion.div
-      animate={{
-        backgroundColor: "rgba(0,0,0,0)",
-        width: WIDTH
-      }}
+    <div
       style={Object.assign({}, styles.container, {
         height: HEIGHT - (imageHeight + 100),
         position: "absolute",
         top: HEIGHT - (HEIGHT - (imageHeight + 100)),
+        // border: "1px solid red",
+        width: WIDTH,
         left: 0
         // backgroundColor: nightmode ? `rgb(16,27,38)` : `rgb(0,153,229)`
       })}
     >
       <motion.div
-        initial="hidden"
-        animate="visible"
-        transition={{ delay: 600 }}
+        animate={{
+          backgroundColor: nightmode ? `rgb(236,183,49)` : `rgb(0,153,229)`
+        }}
+        // transition={{ delay: 600 }}
         style={{
           height: 100,
           width: imageWidth,
-          backgroundColor: nightmode ? `#ecb731` : `rgb(0,153,229)`,
+
           borderRadius: 5,
           display: "flex",
           justifyContent: "center",
@@ -47,7 +46,7 @@ const GetProduct: FC = () => {
           }}
         />
       </motion.div>
-    </motion.div>
+    </div>
   );
 };
 
